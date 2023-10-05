dc7564709d39ba9c88b47f721c6f926a86d71f23
weiwei-dotcom <728841808@qq.com>
2023-10-05 22:27:06 +0800

    update launch file and .vscode file

--- src/slam/launch/slam.launch.py
+++ src/slam/launch/slam.launch.py
@@ -3,18 +3,22 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     
+    usb_cam = Node(
+        package = "usb_cam",
+        executable="usb_cam_node_exe"
+    )
     slam = Node(
-        package="slam",
-        executable="slam"
+        package="orbslam",
+        executable="orbslam"
     )
     media = Node(
         package="media",
         executable="media"
     )
     model = Node(
-        package="model3",
-        executable="model3"
+        package="model",
+        executable="model"
     )
     
-    launch_description = LaunchDescription([slam,media,model])
+    launch_description = LaunchDescription([usb_cam,slam,media,model])
     return launch_description
