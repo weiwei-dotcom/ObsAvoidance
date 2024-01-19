5f25be82e5760ab2bc66aff615f8f25972228914
weiwei-dotcom <728841808@qq.com>
2024-01-19 17:45:38 +0800

    .

--- src/path_planner/src/planner.cpp
+++ src/path_planner/src/planner.cpp
@@ -3,7 +3,7 @@
 PathPlanner::PathPlanner():Node("path_planner")
 {
     flag_get_grid_map = false;
-
+    flag_get_plan_start = false;
     this->pcl_obs_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>("pcl_obstacle",10,std::bind(&PathPlanner::pclObsCallback,this,std::placeholders::_1));
 
     this->declare_parameter<std::double_t>("replan_period", 0.5);
@@ -32,6 +32,29 @@ PathPlanner::PathPlanner():Node("path_planner")
 
     this->declare_parameter<int16_t>("inflation_radius",5);
     this->inflation_radius = this->get_parameter("inflation_radius").as_int();
+
+    this->declare_parameter<double_t>("start_position_x", 1000.0);
+    this->declare_parameter<double_t>("start_position_y", 1900.0);
+    this->declare_parameter<double_t>("start_position_z", 1000.0);
+    this->declare_parameter<double_t>("start_speed_x", 0.0);
+    this->declare_parameter<double_t>("start_speed_y", 20.0);
+    this->declare_parameter<double_t>("start_speed_z", 0.0);
+    this->start_position.x() = this->get_parameter("start_position_x").as_double();
+    this->start_position.y() = this->get_parameter("start_position_y").as_double();
+    this->start_position.z() = this->get_parameter("start_position_z").as_double();
+    this->start_speed.x() = this->get_parameter("start_speed_x").as_double();
+    this->start_speed.y() = this->get_parameter("start_speed_y").as_double();
+    this->start_speed.z() = this->get_parameter("start_speed_z").as_double();
+
+    this->declare_parameter<double_t>("init_proj_map_lati_step", 1.0);
+    this->declare_parameter<double_t>("init_proj_map_longi_step", 2.0);
+    this->declare_parameter<double_t>("opt_proj_map_lati_step", 1.0);
+    this->declare_parameter<double_t>("opt_proj_map_longi_step", 2.0);
+    this->init_proj_map_lati_size = std::ceil(90.0/this->get_parameter("init_proj_map_lati_step").as_double());
+    this->init_proj_map_longi_size = std::ceil(360.0/this->get_parameter("init_proj_map_longi_step").as_double());
+    this->opt_proj_map_lati_size = std::ceil(90.0/this->get_parameter("opt_proj_map_lati_step").as_double());
+    this->opt_proj_map_longi_size = std::ceil(360.0/this->get_parameter("opt_proj_map_longi_step").as_double());
+
     return;
 }
 
@@ -42,7 +65,6 @@ void PathPlanner::pclObsCallback(const sensor_msgs::msg::PointCloud2::SharedPtr 
     {
         return;        
     }
-
     pcl::fromROSMsg(*pcl_obs_msg, this->pcl_obs);
     int pcl_obs_size = pcl_obs.points.size();
     int temp_index_x,temp_index_y,temp_index_z;
@@ -51,9 +73,7 @@ void PathPlanner::pclObsCallback(const sensor_msgs::msg::PointCloud2::SharedPtr 
         temp_index_x = floor((pcl_obs.points[i].x-this->grid_map_origin_point.x())/this->resolution);
         temp_index_y = floor((pcl_obs.points[i].y-this->grid_map_origin_point.y())/this->resolution);
         temp_index_z = floor((pcl_obs.points[i].z-this->grid_map_origin_point.z())/this->resolution);
-
         this->occupy_status[temp_index_x][temp_index_y][temp_index_z] = true;
-
         for (int x=temp_index_x-inflation_radius;x<=temp_index_x+inflation_radius;x++)
         {
             for (int y=temp_index_y-inflation_radius;y<=temp_index_y+inflation_radius;y++)
@@ -73,7 +93,22 @@ void PathPlanner::pclObsCallback(const sensor_msgs::msg::PointCloud2::SharedPtr 
 // init the straight line path that for the 
 void PathPlanner::replanPathCallback()
 {
-    //TODO:
+    //TODO: 
+    if (!this->flag_get_grid_map || !flag_get_plan_start) return; // this plan start position and velocity is got from the 
+                                                                  // decoder of the linear mechanism's motor
+    // initializing the straight line path
+    // start to project the obstacle to the project map
+    for (int i=0; i<this->init_proj_map_lati_size;i++)
+
+    // initializing the B spline control point on the straight line according to the preconfigured interval distance 
+
+    // use the sample point of B spline to detect collision,
+        // if collision
+        // record the time knot and position of start & end sample point of the every collision segment 
+        // use the vector<vector<int>> collision_indexs_list record the index of control point list of every collision 
+        // segment accord the time knot,
+        // (each vector<int> of collision_indexs_list store a set of index of control point of a collision segment) 
+    
     return;
 }
 
--- src/path_planner/src/planner.hpp
+++ src/path_planner/src/planner.hpp
@@ -9,22 +9,38 @@
 #include "pcl_conversions/pcl_conversions.h"
 #include "pcl/common/transforms.h"
 #include <pcl/point_types.h>
-// #include <ceres/ceres.h>
+#include <ceres/ceres.h>
 
 
 class PathPlanner:public rclcpp::Node
 {
 private:
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pcl_obs_sub;
+
     rclcpp::TimerBase::SharedPtr replan_timer;
+
     pcl::PointCloud<pcl::PointXYZ> pcl_obs;
+
     int grid_map_x_size,grid_map_y_size,grid_map_z_size;
+
     std::vector<std::vector<std::vector<bool>>> occupy_status;
+
     double resolution;
+
     int inflation_radius;
-    bool flag_get_grid_map;
+
+    bool flag_get_grid_map,flag_get_plan_start;
+
     Eigen::Vector3d grid_map_origin_point;
+
     double replan_period;
+
+    Eigen::Vector3d start_position,start_speed;
+
+    std::vector<std::vector<Eigen::Vector3d>> control_points_list;
+
+    int init_proj_map_lati_size, init_proj_map_longi_size;
+    int opt_proj_map_lati_size, opt_proj_map_longi_size;
 
 public:
     PathPlanner();
